e7a73185bd0397631cc22aa8ccac3d7736575a06
LubouLukashevich <lukashevich.rs@gmail.com>
2023-02-07 15:21:24 +0300

    fix: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+       this.min;
+       this.max;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return Math.ceil((this.min + this.max)/2);   
     }
 
     lower() {
-
+        this.max =  Math.ceil((this.min + this.max)/2); 
     }
 
     greater() {
-
+        this.min = Math.ceil((this.min + this.max)/2); 
     }
 }
 
